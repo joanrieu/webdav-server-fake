eb4866332c4b1231bf8296b28c1c454bd59a5b3c
Joan Rieu <joan@joanrieu.com>
2018-09-09 19:14:56 +0200

    code cleanup

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const assert = require("assert");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 
-const DIR = '..'
- 
+const DIR = process.argv[2] || process.cwd();
+
 const app = express();
-app.use(function (req, res, next) {
-	next();
-	console.log(req.method + " " + req.url + " ==> " + res.statusCode);
-});
+
+// log requests
+app.use(morgan("tiny"));
+
+// allow cross-domain requests
 app.use(cors());
-app.use(express.static(DIR));
-app.put('/:file', bodyParser.raw(), (req, res) => {
-	res.send(fs.writeFileSync(path.join(DIR, req.params.file), req.body));
+
+// read files (and provide Last-Modified header)
+app.all("/:file", express.static(DIR));
+
+// write files
+app.put("/:file", bodyParser.raw(), (req, res) => {
+  const filePath = path.join(DIR, req.params.file);
+  fs.writeFileSync(filePath, req.body);
+  res.send();
 });
+
 app.listen(8080);
